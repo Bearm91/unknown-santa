0488f2779baf6456207d011635f9fb606a7ad2e7
Beatriz <bearm91@gmail.com>
2020-02-09 13:05:37 +0100

    sendEmail() test

---
+++ app/src/main/java/com/bearm/unknownsanta/Activities/EmailSender.java
@@ -0,0 +1,27 @@
+package com.bearm.unknownsanta.Activities;
+
+import android.content.Intent;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EmailSender extends AppCompatActivity {
+
+    public EmailSender() {
+
+    }
+
+
+    public void sendEmail(){
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/rfc822");
+        i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"recipient@example.com"});
+        i.putExtra(Intent.EXTRA_SUBJECT, "subject of email");
+        i.putExtra(Intent.EXTRA_TEXT   , "body of email");
+        try {
+            startActivity(Intent.createChooser(i, "Send mail..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(EmailSender.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
